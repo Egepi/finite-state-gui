5cd5a2cea017ac0aff68a1fc9480ae8c6c53da84
Todd.Silvia <Todd.Silvia@442fa55c-2bba-11df-ae7a-2764ac86b46b>
2010-03-10 14:49:47 +0000

    Deleted some code that was previously commented

--- trunk/ finite-state-gui/JFLAP.java
+++ trunk/ finite-state-gui/JFLAP.java
@@ -15,34 +15,6 @@
  */
 
 
-
-
-
-import file.Codec;
-import file.ParseException;
-import file.xml.Transducer;
-import file.xml.TransducerFactory;
-import gui.action.NewAction;
-import gui.action.OpenAction;
-import gui.environment.Profile;
-import gui.environment.Universe;
-
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-
-//ORG//import org.w3c.dom.Document;
-//ORG//import org.w3c.dom.Node;
-//ORG//import org.w3c.dom.NodeList;
-//ORG//import org.xml.sax.SAXException;
-
 /**
  * This is the class that starts JFLAP.
  * 
