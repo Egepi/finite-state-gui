0dafe74ea3e11b1bf25ff570fe64b07a3636bbc1
jenniferk89 <jenniferk89@442fa55c-2bba-11df-ae7a-2764ac86b46b>
2010-04-18 04:29:05 +0000

    parses multiple .jff files

--- trunk/parser/fsmguiParser.py
+++ trunk/parser/fsmguiParser.py
@@ -1,28 +1,42 @@
-# FSMGUI XML to Python Parser
+# FSMGUI XML ('jff') to Python Parser
 # Author: Jennifer Kinahan
 
 import pprint
 import os
+import xml
+import sys
+import re
+import glob
  
 import xml.dom.minidom
 from xml.dom.minidom import Node
 
-s = "Please type which file you would like to parse within your FSMGUI folder: \nExample: 'books.xml'\n\n"
-filename = raw_input(s)
-
-print "Searching for:",filename, "\n"
-
-try:                          # wrap "dangerous" code in a try block
-  doc = xml.dom.minidom.parse(filename)
+#Path of the folder where all the files are stored.
+#Note: CHANGE THIS TO INCORPORATE THE XML FILES YOU WANT TO BE PARSED
+path = 'C:\Users\YT\Documents\EVL\ProjectLifelike\FSMGUI\parser'
+# parse through all .xml documents within given path
+for infile in glob.glob(os.path.join(path, '*.jff')):
+    
+    # infile stores the complete path of the file
+    print "Current File Being Processed is:  " + infile 
+    #use split, display both path and filename of files being parsed
+    (PATH, FILENAME) = os.path.split(infile)
+    print " PATH is " + PATH
+    print " FILENAME is " + FILENAME
+	
+try:                          
+  doc = xml.dom.minidom.parse(infile)
 except IOError:               # catch IOError and deriving exceptions
   print "Could not open\n"
   os._exit(99)
 else:                         # optional, run block if no exception thrown
-  print "Parsing:",filename, "\n"
+  print "Parsing:",FILENAME, "\n"
   
 mapping = {}
   
 for node in doc.getElementsByTagName("state"):
+  id = node.getAttribute("id")
+  name = node.getAttribute("name")
   stateidle = node.getElementsByTagName("stateIdleResp")
   label = node.getElementsByTagName("label")
   for node2 in stateidle:
@@ -76,5 +90,4 @@ for node in doc.getElementsByTagName("transition"):
   mapping[response] = "resp: " + response
   mapping[keyword] = "key: " + keyword
   mapping[read] = "read: " + read		
-  
 pprint.pprint(mapping)
